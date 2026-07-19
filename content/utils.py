2630a5d4f1f819aaa2cb059b70a43ee28c5be4ba
Penelope Gkinala <penelope_gkinala@protonmail.com>
2026-07-19 14:08:12 +0300

    fixed premium user access for premium authors

--- content/utils.py
+++ content/utils.py
@@ -5,6 +5,8 @@ from .models import Unlock
 def has_access(user, writeup):
     if not writeup.is_premium:
         return True
+    if writeup.author_id == user.id:
+        return True
     if Unlock.objects.filter(user=user, writeup=writeup).exists():
         return True
     if Subscription.objects.filter(user=user, expires_at__gte=timezone.now()).exists():
